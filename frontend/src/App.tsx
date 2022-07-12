4f97e4f534e0829603df4b92f86b36638ac684c6
ricardoazcosta <ricardozanete@gmail.com>
2022-07-11 21:24:38 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./componentes/NotificationButton"
 
 function App() {
   return(
-    <h1>Olá Mundo</h1>
+    <>
+    <h1>Olá Mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
