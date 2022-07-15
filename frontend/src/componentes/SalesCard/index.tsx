a9edec187176acd954c04096056baff765aba70a
ricardoazcosta <ricardozanete@gmail.com>
2022-07-15 18:59:11 -0300

    Send notification

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -23,7 +23,7 @@ useEffect(() => {
 
   const dmin = minDate.toISOString().slice(0, 10);
   const dmax = maxDate.toISOString().slice(0, 10);
-  console.log(dmin);
+  
 
   axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
   .then(response => {
@@ -82,7 +82,7 @@ useEffect(() => {
                     <td>R$ {sale.amount.toFixed(2)}</td>
                     <td>
                       <div className="dsmeta-red-btn-container">
-                        <NotificationButton />
+                        <NotificationButton saleId={sale.id}/>
                       </div>
                     </td>
                   </tr>
